32f6e67c5b2b7ad3062a462975509ec7e3c1e86a
diferarce91 <diferarce91@gmail.com>
2022-10-10 15:35:51 -0500

    initial commit

---
+++ src/main/java/com/globan/knowledgeChallenge/KnowledgeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.globan.knowledgeChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globan/knowledgeChallenge/KnowledgeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globan.knowledgeChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
